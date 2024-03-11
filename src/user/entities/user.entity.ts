255e5fd9030144c5022632f291ae8d22e47b48e0
wanhee27 <chowh727@gmail.com>
2024-03-12 06:44:12 +0900

    ✨ Feat
    User entity 추가

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Role } from '../types/userRole.type';
+import { Reservation } from 'src/reservation/entities/reservation.entity';
+
 export class User {}
+
+@Entity()
+export class Customer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: false, select: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: Role, nullable: false })
+  role: Role;
+
+  @Column({ type: 'int', nullable: false, default: 1000000 })
+  point: Date;
+
+  @OneToMany(() => Reservation, (reservation) => reservation, { cascade: true })
+  ticket: Reservation[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
